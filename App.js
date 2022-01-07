c58be51b32c48a12f5884f17c334ceed76f0e0f5
Lakshmi Ganesan <93819132+Lakshmi-Ganesan@users.noreply.github.com>
2022-01-07 17:40:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+import RouterComponent from './RouterComponent'
+
+export default function App() {
+  return (
+    <><div>
+      
+    <RouterComponent/>
+    </div>
+    </>
+  );
+}
